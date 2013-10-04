3d38b759fb1a024cf01295f513e774e3833bf13f
Mitja Uršič <odtihmal@gmail.com>
2013-10-04 09:30:21 +0200

    Split function.

--- renderer.js
+++ renderer.js
@@ -12,45 +12,161 @@ document.body.appendChild(renderer.domElement);
 
 var geom = new THREE.Geometry();
 
-var v0 = new THREE.Vector3(-floorSize / 2, 0, -floorSize / 2);
-var v1 = new THREE.Vector3(0, 0, -floorSize / 2);
-var v2 = new THREE.Vector3(floorSize / 2, 0, -floorSize / 2);
+var f_index = 0;
+var m_points = [];
+var f_vertices = [];
+var f_faces = [];
+var n_faces = [];
 
-var v3 = new THREE.Vector3(-floorSize / 2, 0, 0);
-var v4 = new THREE.Vector3(0, 0, 0);
-var v5 = new THREE.Vector3(floorSize / 2, 0, 0);
+function add_vertex(v)
+{
+    f_vertices.push(v);
+    geom.vertices.push(v);
+    return f_index++;
+}
 
-var v6 = new THREE.Vector3(-floorSize / 2, 0, floorSize / 2);
-var v7 = new THREE.Vector3(0, 0, floorSize / 2);
-var v8 = new THREE.Vector3(floorSize / 2, 0, floorSize / 2);
+function add_face(f) {
+    f_faces.push(f);
+    geom.faces.push(new THREE.Face3(f.lv, f.rv, f.tv));
+}
 
-geom.vertices.push(v0);
-geom.vertices.push(v1);
-geom.vertices.push(v2);
+function add_face(f) {
+    f_faces.push(f);
+    geom.faces.push(new THREE.Face3(f.lv, f.rv, f.tv));
+}
 
-geom.vertices.push(v3);
-geom.vertices.push(v4);
-geom.vertices.push(v5);
+function get_middle_point(p1, p2) {
+    var first_is_smaller, smaller_index,
+        greater_index, key, k, v1, v2, i;
+    first_is_smaller = p1 < p2;
+    smaller_index = first_is_smaller ? p1 : p2;
+    greater_index = first_is_smaller ? p2 : p1;
+    key = (smaller_index << 32) + greater_index;
 
-geom.vertices.push(v6);
-geom.vertices.push(v7);
-geom.vertices.push(v8);
+    for (k in m_points) {
+        if (key === m_points[k]) {return key;}
+    }
 
-console.log(geom.vertices);
+    v1 = f_vertices[p1];
+    v2 = f_vertices[p2];
 
-// geom.faces.push(new THREE.Face3(0, 1, 2));
+    i = add_vertex(new THREE.Vector3((v1.x + v2.x) / 2,
+                                     0,
+                                     (v1.z + v2.z) / 2));
 
-geom.faces.push(new THREE.Face3(3, 1, 0)); // T0
-geom.faces.push(new THREE.Face3(1, 3, 4)); // T1
+    m_points.push({key: key, pos: i});
 
-geom.faces.push(new THREE.Face3(4, 2, 1)); // T2
-geom.faces.push(new THREE.Face3(2, 4, 5)); // T3
+    return i;
+}
 
-geom.faces.push(new THREE.Face3(6, 4, 3)); // T4
-geom.faces.push(new THREE.Face3(4, 6, 7)); // T5
+function remove_face(f)
+{
+    f_faces = _.without(f_faces, f);
+}
 
-geom.faces.push(new THREE.Face3(7, 5, 4)); // T6
-geom.faces.push(new THREE.Face3(5, 7, 8)); // T7
+function split(f_index, origin) {
+    var f = f_faces[f_index];
+    var faces = [];
+    var n_faces = [];
+    var tn0, tn1, lcn, rcn;
+    
+    faces = split_once(f);
+
+    tn0 = faces[0];
+    tn0 = faces[0];
+
+    n_faces = faces;
+
+    if (origin) {
+        if (f.lcn && f.lcn === origin) {
+            lcn = [];
+            lcn = split_once(faces[0]);
+            faces = [faces[1]];
+            f_faces = f_faces.concat(lcn);
+            n_faces = JSON.parse(JSON.stringify(lcn));
+            n_faces.push(faces[1]);
+        }
+
+        if(f.rcn && f.rcn == origin) {
+            rcn = [];
+            rcn = split_once(faces[1]);
+            faces = [faces[0]];
+            f_faces = f_faces.concat(lcn);
+            n_faces = JSON.parse(JSON.stringify(rcn));
+            n_faces.push(faces[0]);
+        }
+    }
+
+    remove_face(f);
+    f_faces = f_faces.concat(faces);
+    neighbors[f_index] = n_faces;
+
+    if (f.hypn && (f.hypn != origin)) {
+        split(f.hypn, f);
+    }
+
+    return faces;
+}
+
+var v0 = add_vertex(new THREE.Vector3(-floorSize / 2, 0, -floorSize / 2));
+var v1 = add_vertex(new THREE.Vector3(0, 0, -floorSize / 2));
+var v2 = add_vertex(new THREE.Vector3(floorSize / 2, 0, -floorSize / 2));
+
+var v3 = add_vertex(new THREE.Vector3(-floorSize / 2, 0, 0));
+var v4 = add_vertex(new THREE.Vector3(0, 0, 0));
+var v5 = add_vertex(new THREE.Vector3(floorSize / 2, 0, 0));
+
+var v6 = add_vertex(new THREE.Vector3(-floorSize / 2, 0, floorSize / 2));
+var v7 = add_vertex(new THREE.Vector3(0, 0, floorSize / 2));
+var v8 = add_vertex(new THREE.Vector3(floorSize / 2, 0, floorSize / 2));
+
+var t0 = {lv: v3, rv: v1, tv: v0};
+var t1 = {lv: v1, rv: v3, tv: v4};
+
+var t2 = {lv: v4, rv: v2, tv: v1};
+var t3 = {lv: v2, rv: v4, tv: v5};
+
+var t4 = {lv: v6, rv: v4, tv: v3};
+var t5 = {lv: v4, rv: v6, tv: v7};
+
+var t6 = {lv: v7, rv: v5, tv: v4};
+var t7 = {lv: v5, rv: v7, tv: v8};
+
+// Neighbors and their relative sizes.
+t0.lcn = null; t0.rcn = null; t0.hypn = t1;
+t0.name = "t0";
+
+t1.lcn = t2; t1.rcn = t4; t1.hypn = t0;
+t1.name = "t1";
+
+t2.lcn = t1; t2.rcn = null; t2.hypn = t3;
+t2.name = "t2";
+
+t3.lcn = null; t3.rcn = t6; t3.hypn = t2;
+t3.name = "t3";
+
+t4.lcn = null; t4.rcn = t1; t4.hypn = t5;
+t4.name = "t4";
+
+t5.lcn = t6; t5.rcn = null; t5.hypn = t4;
+t5.name = "t5";
+
+t6.lcn = t5; t6.rcn = t3; t6.hypn = t7;
+t6.name = "t6";
+
+t7.lcn = null; t7.rcn = null; t7.hypn = t6;
+t7.name = "t7";
+
+add_face(t0);
+add_face(t1);
+add_face(t2);
+add_face(t3);
+add_face(t4);
+add_face(t5);
+add_face(t6);
+add_face(t7);
+
+console.log("x ", t0);
 
 geom.computeFaceNormals();
 
@@ -74,12 +190,19 @@ var key_codes = {KEY_UP: 38,
                  KEY_p: 80,
                  KEY_t: 84};
 
-var render = function() {
-    requestAnimationFrame(render);
-    renderer.render(scene, camera);
-};
+var rotation_commands = {KEY_UP:     {axis: 'x', step:  0.02},
+                         KEY_DOWN:   {axis: 'x', step: -0.02},
+                         KEY_LEFT:   {axis: 'y', step:  0.02},
+                         KEY_RIGHT:  {axis: 'y', step: -0.02},
+                         KEY_PGUP:   {axis: 'z', step:  0.02},
+                         KEY_PGDOWN: {axis: 'z', step: -0.02}};
 
-render();
+var translation_commands = {KEY_a: {axis: 'x', step:  14},
+                            KEY_d: {axis: 'x', step: -14},
+                            KEY_w: {axis: 'y', step:  14},
+                            KEY_s: {axis: 'y', step: -14},
+                            KEY_e: {axis: 'z', step:  14},
+                            KEY_q: {axis: 'z', step: -14}};
 
 function rotate_camera(axis, how_much) {
     camera.rotation[axis] += how_much;
@@ -101,23 +224,6 @@ function reset_camera_translation() {
     camera.position.z = 1210;
 }
 
-var rotation_commands = {KEY_UP: {axis: 'x', step: 0.02},
-                         KEY_DOWN: {axis: 'x', step: -0.02},
-                         KEY_LEFT: {axis: 'y', step: 0.02},
-                         KEY_RIGHT: {axis: 'y', step: -0.02},
-                         KEY_PGUP: {axis: 'z', step: 0.02},
-                         KEY_PGDOWN: {axis: 'z', step: -0.02}};
-
-var translation_commands = {KEY_a: {axis: 'x', step: 14},
-                            KEY_d: {axis: 'x', step: -14},
-                            KEY_w: {axis: 'y', step: 14},
-                            KEY_s: {axis: 'y', step: -14},
-                            KEY_e: {axis: 'z', step: 14},
-                            KEY_q: {axis: 'z', step: -14}};
-
-reset_camera_rotation();
-reset_camera_translation();
-
 document.addEventListener
 ('keydown', function(e)
  {
@@ -130,7 +236,6 @@ document.addEventListener
          }
 
          which = translation_commands[_.invert(key_codes)[e.keyCode]];
-
          if (which) {
              translate_camera(which.axis, which.step);
          }
@@ -166,3 +271,17 @@ document.addEventListener
          camera.rotation.z = 0;
      }
  });
+
+for (k in geom.vertices) {
+    f_vertices.push(geom.vertices[k]);
+}
+
+reset_camera_rotation();
+reset_camera_translation();
+
+var render = function() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+};
+
+render();
